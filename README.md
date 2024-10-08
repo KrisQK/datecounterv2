154989aa8b563e00f31a9cc201865eb69c9424de
Kris Qikun Li - Mac <qikunli8866+mac@gmail.com>
2024-10-09 01:34:09 +1300

    practice controled element in react

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![alt "my show time"](./image.png)
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./styles.css";
+// const { DateTime } = require("luxon");
 
 export default function App() {
     return (
@@ -10,16 +11,14 @@ export default function App() {
 }
 
 function Counter() {
-    const [count, setCount] = useState(0);
     const [step, setStep] = useState(1);
 
-    function handleReset() {
-        setCount(0);
-        setStep(1);
-    }
-
-    const date = new Date("june 21 2027");
+    const [count, setCount] = useState(0);
+    console.log(count);
+    const date = new Date("2027-6-1");
+    console.log(date);
     date.setDate(date.getDate() + count);
+    console.log(date);
 
     return (
         <div>
@@ -30,36 +29,81 @@ function Counter() {
                     max="10"
                     value={step}
                     onChange={e => setStep(Number(e.target.value))}
-                />
-                <span>Step: {step}</span>
+                ></input>
+                {step}
             </div>
-
             <div>
                 <button onClick={() => setCount(c => c - step)}>-</button>
                 <input
                     type="text"
                     value={count}
                     onChange={e => setCount(Number(e.target.value))}
-                />
+                ></input>
                 <button onClick={() => setCount(c => c + step)}>+</button>
             </div>
-
             <p>
                 <span>
                     {count === 0
                         ? "Today is "
                         : count > 0
-                        ? `${count} days from today is `
-                        : `${Math.abs(count)} days ago was `}
+                        ? `${count} days from today is`
+                        : `${Math.abs(count)} days ago today was`}
                 </span>
-                <span>{date.toDateString()}</span>
+                <span> {date.toDateString()}</span>
             </p>
-
-            {count !== 0 || step !== 1 ? (
-                <div>
-                    <button onClick={handleReset}>Reset</button>
-                </div>
-            ) : null}
+            <button
+                onClick={() => {
+                    setCount(0);
+                    setStep(1);
+                }}
+            >
+                reset
+            </button>
         </div>
     );
 }
+
+/*
+function Counter() {
+    const [step, setStep] = useState(1);
+
+    const [count, setCount] = useState(0);
+    const [nDate, setNDate] = useState(new Date("2027-06-01"));
+ 
+
+    function handlerEvent1() {
+        setCount(c => c + step);
+        setNDate(d => {
+            const newDate = new Date(d);
+            newDate.setDate(newDate.getDate() + step);
+            return newDate;
+        });
+    }
+    function handlerEvent2() {
+        setCount(c => c - step);
+        setNDate(d => {
+            const newDate = new Date(d);
+            newDate.setDate(newDate.getDate() - step);
+            return newDate;
+        });
+    }
+    // console.log(newDate);
+    return (
+        <div>
+            <div>
+                <button onClick={() => setStep(s => s - 1)}>-</button>
+                Step: {step}
+                <button onClick={() => setStep(s => s + 4)}>+</button>
+            </div>
+            <div>
+                <button onClick={handlerEvent2}>-</button>
+                Count: {count}
+                <button onClick={handlerEvent1}>+</button>
+            </div>
+            <p>
+                {count} days from today is {nDate.toDateString()}
+            </p>
+        </div>
+    );
+}
+*/
